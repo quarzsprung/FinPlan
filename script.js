daed0fd30230c42a6ee5c2dc1d1f10fc6346e5ec
quarzsprung <quarzsprung@tutanota.com>
2022-01-28 21:29:40 +0100

    added the logic for summarizing and displaying the incomes

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 function addEinnahme() {
 
-    einnahmenliste.innerHTML += `<li class="mdl-list__item">
-      <span class="mdl-list__item-primary-content">
-        <i class="material-icons  mdl-list__item-avatar">add_circle_outline</i>
-        ${einnahme.value}
-      </span>
-      <span class="mdl-list__item-primary-content">
-        <i class="material-icons  mdl-list__item-avatar">euro</i>
-        ${einnahmebetrag.value}
-      </span>
-    </li>`;
+  einnahmenliste.innerHTML += `<li class="mdl-list__item">
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons  mdl-list__item-avatar">add_circle_outline</i>
+      ${einnahme.value}
+    </span>
+    <span class="mdl-list__item-primary-content">
+      <i class="material-icons  mdl-list__item-avatar">euro</i>
+      ${einnahmebetrag.value}
+    </span>
+  </li>`
 }
 
-function addAusgabe() {
+var myArray = [];
+var sum = 0;
 
-    einnahmenliste.innerHTML += `<li class="mdl-list__item">
-      <span class="mdl-list__item-primary-content">
-        <i class="material-icons  mdl-list__item-avatar">add_circle_outline</i>
-        ${ausgabe.value}
-      </span>
-      <span class="mdl-list__item-primary-content">
-        <i class="material-icons  mdl-list__item-avatar">euro</i>
-        ${ausgabebetrag.value}
-      </span>
-    </li>`;
+function addtoArray() {
+
+  var test = parseInt(document.getElementById("einnahmebetrag").value);
+  myArray.push(test);
+  console.log(myArray);
+
+  sum = myArray.reduce((a, b) => a + b, 0);
+  console.log(sum);
+
+  document.getElementById("displayEinnahmen").value = sum;
 }
+
